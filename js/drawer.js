ad9c05c3f0da52ba14b803bd8f1834eeb9438834
AMJones <aaronj@jonesiscoding.com>
2017-10-26 08:58:07 -0500

    Moves drawer to directory to prep for component extraction.

---
+++ js/drawer.js
@@ -0,0 +1,75 @@
+/**
+ * jQuery Plugin for managing a navigation drawer.
+ *
+ * @version v1.2.0
+ * @license https://github.com/strapless/strapless/LICENSE
+ * @author  Aaron M Jones <am@jonesiscoding.com>
+ */
+(function( $ ){
+
+  $.fn.responsiveDrawer = function() {
+
+    return this.each( function () {
+      var drawer   = this;
+      var $trigger = $( this );
+      var href     = $trigger.attr( 'href' );
+      var $target  = $( $trigger.attr( 'data-target' ) || (href && href.replace( /.*(?=#[^\s]+$)/, '' )) ); // strip for ie7
+      var $html    = $('html');
+
+      drawer.isTemporary = function() {
+        return $.fn.isBreakpoint( [ 'xs', 'sm' ] ) || ($.fn.isBreakpoint(['md']) && $('html').hasClass('touch'));
+      };
+
+      drawer.isPersistent = function() {
+        return $.fn.isBreakpoint(['md']) && !drawer.isTemporary();
+      };
+
+      drawer.init = function() {
+        if ( typeof $target !== 'undefined' ) {
+          if(drawer.isTemporary()) {
+            $html.addClass('temporary').removeClass('on persistent permanent');
+          } else if(drawer.isPersistent()) {
+            if(!$html.hasClass('temporary')) {
+              $html.addClass('on');
+            }
+            $html.addClass('persistent').removeClass('temporary permanent');
+          } else {
+            $html.addClass('permanent').removeClass('temporary persistent on');
+          }
+        }
+      };
+
+      drawer.collapseGroup = function($heading) {
+        var $group = $heading.parent('.list-group');
+        if(!$group.data('bs.collapse')) {
+          var height = $heading.outerHeight() - $group.marginBottom;
+          $group.css('min-height',height).addClass('collapse');
+          $group.collapse('toggle');
+        } else {
+          $group.collapse('toggle');
+        }
+      };
+
+      $trigger.on( 'click', function ( e ) {
+        if ( $trigger.is( 'a' ) ) { e.preventDefault(); }
+        if ( typeof $target !== 'undefined' ) {
+          if ( drawer.isTemporary() ) {
+            $html.toggleClass('on');
+          } else if (drawer.isPersistent() ) {
+            $html.toggleClass('on');
+          }
+        }
+      } );
+
+      $('body').on('click.collapse-group.data-api', '[data-toggle=collapse-group]', function (e) {
+        e.preventDefault();
+        drawer.collapseGroup($(this));
+      });
+
+      $(window).afterwards('resize',function() { drawer.init(); });
+
+      drawer.init();
+
+    } );
+  };
+})( jQuery );
