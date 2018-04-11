73344547ba46dd6c645740d60032e676765c6c9b
AMJones <aaronj@jonesiscoding.com>
2018-04-11 09:09:10 -0500

    Fixes issues with XL drawer on touch displays.

--- dist/js/drawer.js
+++ dist/js/drawer.js
@@ -62,7 +62,7 @@ jQuery.fn.extend( {
 /**
  * jQuery Plugin for managing a navigation drawer.
  *
- * @version v2.0.1
+ * @version v2.0.3
  * @license https://github.com/strapless/strapless/LICENSE
  * @author  Aaron M Jones <am@jonesiscoding.com>
  */
@@ -76,14 +76,42 @@ jQuery.fn.extend( {
       var href     = $trigger.attr( 'href' );
       var $target  = $( $trigger.attr( 'data-target' ) || (href && href.replace( /.*(?=#[^\s]+$)/, '' )) ); // strip for ie7
       var $html    = $('html');
+      var bps      = ['xs','sm','md','lg','xl'];
+      var types    = [ 'temporary', 'persistent', 'permanent' ];
 
       drawer.isTemporary = function() {
-        return $.fn.isBreakpoint( [ 'xs', 'sm', 'md' ] ) || ($.fn.isBreakpoint(['lg']) && $html.hasClass('touch'));
+        return true === ( getDrawerType() === 'temporary' );
       };
 
       drawer.isPersistent = function() {
-        return ($.fn.isBreakpoint(['lg']) || ($.fn.isBreakpoint(['xl']) && $html.hasClass('touch'))) && !drawer.isTemporary();
+        return true === ( getDrawerType() === 'persistent' );
       };
+
+      function getViewport() {
+        for(var x = 0; x < bps.length; x++) {
+          if ( $.fn.isBreakpoint( bps[ x ] ) ) {
+            return bps[x];
+          }
+        }
+
+        return 'xs'
+      }
+
+      function getDrawerType() {
+        var type = 'temporary';
+        var viewport = getViewport();
+        var infix;
+        for(var i = 0; i < bps.length; i++) {
+          infix = (i === 0) ? '' : bps[i] + '-';
+          for(var x = 0; x < types.length; x++) {
+            if ( $target.hasClass( 'drawer-' + infix + types[ x ] ) ) {
+              type = types[x];
+            }
+          }
+
+          if ( bps[ i ] === viewport ) { return type; }
+        }
+      }
 
       drawer.init = function() {
         if ( typeof $target !== 'undefined' ) {
